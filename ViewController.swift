650ae49c1d2ccc8e74efebeaec181f4f8fd129d9
Tiago Silva <1140437@isep.ipp.pt>
2017-12-10 20:12:45 +0000

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Window-Shopper
+//
+//  Created by Tiago Santos on 10/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
